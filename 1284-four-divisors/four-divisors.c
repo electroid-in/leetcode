96f6d13626c39dcde312b4552bb72b9821ef7631
Adhithyan S <109207875+electroid-in@users.noreply.github.com>
2026-01-04 15:42:53 +0530

    Time: 1 ms (95.45%) | Memory: 8.9 MB (9.09%) - LeetSync

---
+++ 1284-four-divisors/four-divisors.c
@@ -0,0 +1,34 @@
+#include <math.h>
+
+int sumFourDivisors(int* nums, int numsSize) {
+    int totalSum = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+        int n = nums[i];
+        int count = 0;
+        int sum = 0;
+
+        for (int d = 1; d * d <= n; d++) {
+            if (n % d == 0) {
+                int other = n / d;
+
+                if (d == other) {
+                    count += 1;
+                    sum += d;
+                } else {
+                    count += 2;
+                    sum += d + other;
+                }
+
+                if (count > 4)
+                    break;
+            }
+        }
+
+        if (count == 4) {
+            totalSum += sum;
+        }
+    }
+
+    return totalSum;
+}
