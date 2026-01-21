2f41d2738670b4c0b72d633a1a36dd088267c21a
Adhithyan S <109207875+electroid-in@users.noreply.github.com>
2026-01-21 11:17:09 +0530

    Time: 0 ms (100.00%) | Memory: 28.4 MB (9.71%) - LeetSync

---
+++ 3611-construct-the-minimum-bitwise-array-ii/construct-the-minimum-bitwise-array-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        vector<int> ans;
+        
+        for (int p : nums) {
+            // Special case: no solution for 2
+            if (p == 2) {
+                ans.push_back(-1);
+                continue;
+            }
+            
+            // Count trailing ones in p
+            int t = 0;
+            int temp = p;
+            while (temp & 1) {
+                t++;
+                temp >>= 1;
+            }
+            
+            // If no trailing ones, no solution (theoretically won't happen for odd primes)
+            if (t == 0) {
+                ans.push_back(-1);
+            } else {
+                // Minimum x
+                int x = p - (1 << (t - 1));
+                ans.push_back(x);
+            }
+        }
+        
+        return ans;
+    }
+};
