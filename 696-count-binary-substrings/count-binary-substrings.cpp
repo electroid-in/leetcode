e98827efa8a9fa19b3dbdee2777a224823bf8ead
Adhithyan S <109207875+electroid-in@users.noreply.github.com>
2026-02-19 11:49:12 +0530

    Time: 4 ms (52.74%) | Memory: 13.1 MB (32.17%) - LeetSync

---
+++ 696-count-binary-substrings/count-binary-substrings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countBinarySubstrings(string s) {
+        int prevGroup = 0;
+        int currGroup = 1;
+        int result = 0;
+        
+        for (int i = 1; i < s.size(); i++) {
+            if (s[i] == s[i - 1]) {
+                currGroup++;
+            } else {
+                result += min(prevGroup, currGroup);
+                prevGroup = currGroup;
+                currGroup = 1;
+            }
+        }
+        
+        // Add last comparison
+        result += min(prevGroup, currGroup);
+        
+        return result;
+    }
+};
